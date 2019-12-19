bcafe4e30f91e5a6ca330da5f6b16f610b00624d
MariusVanDerWijden <m.vanderwijden@live.de>
2019-12-19 13:28:25 +0100

    :recycle: [paymentapp] removed superflous checks

--- contracts/PaymentApp.sol
+++ contracts/PaymentApp.sol
@@ -1,59 +1,53 @@
-// Copyright (c) 2019 The Perun Authors. All rights reserved.
-// This file is part of go-perun. Use of this source code is governed by a
-// MIT-style license that can be found in the LICENSE file.
+// Copyright (c) 2019 Chair of Applied Cryptography, Technische Universität
+// Darmstadt, Germany. All rights reserved. This file is part of go-perun. Use
+// of this source code is governed by a MIT-style license that can be found in
+// the LICENSE file.
 
-pragma solidity 0.5.11;
+pragma solidity ^0.5.13;
 pragma experimental ABIEncoderV2;
 
-import "./PerunTypes.sol";
-import "./ValidTransition.sol";
-import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
+import "./Channel.sol";
+import "./App.sol";
+import "./SafeMath.sol";
 
-contract PaymentApp is ValidTransitioner {
-    
+/**
+ * @title A PaymentApp to be used in a state channel.
+ * @author The Perun Authors
+ * @dev The PaymentApp implements the validTransition function of the App interface.
+ */
+contract PaymentApp is App {
+
     using SafeMath for uint256;
 
-	function validTransition(
-	    PerunTypes.Params calldata params,
-		PerunTypes.State calldata from,
-		PerunTypes.State calldata to) 
-	external pure returns (bool)
-	{
-	    require(from.channelID == to.channelID);
-	    require(from.appData.length == 0);
-	    require(to.appData.length == 0);
-	    require(!from.isFinal);
-	    
-        require(validPayment(from.outcome, to.outcome, from.moverIdx));
-	}
-	
-	function validPayment(
-		PerunTypes.Allocation memory oldAlloc,
-		PerunTypes.Allocation memory newAlloc,
-		uint256 sender)
-	internal pure returns (bool)
-	{
-		assert(oldAlloc.assets.length == newAlloc.assets.length);
-		for (uint256 i = 0; i < newAlloc.assets.length; i++) {
-			require(oldAlloc.assets[i] == newAlloc.assets[i]);
-			uint256 sumOld = 0;
-			uint256 sumNew = 0;
-			assert(oldAlloc.balances[i].length == newAlloc.balances[i].length);
-			for (uint256 k = 0; k < newAlloc.balances[i].length; k++) {
-			    // only sender can send money
-			    if(sender == k)
-			        require(newAlloc.balances[i][k] <= oldAlloc.balances[i][k]);
-			    else 
-			        require(newAlloc.balances[i][k] >= oldAlloc.balances[i][k]);
-			    // calculate sum
-				sumOld = sumOld.add(oldAlloc.balances[i][k]);
-				sumNew = sumNew.add(newAlloc.balances[i][k]);
-			}
-			require(sumOld == sumNew, 'Sum of balances for an asset must be equal');
-		}
-		// SubAlloc's currently not implemented
-		require(oldAlloc.locked.length == 0, 'SubAlloc currently not implemented');
-		require(newAlloc.locked.length == 0, 'SubAlloc currently not implemented');
-		return true;
-	}
+    /**
+     * @notice ValidTransition checks if there exists a valid transition between two states.
+     * @dev ValidTransition should revert on an invalid transition.
+     * @param params The parameters of the channel.
+     * @param from The current state.
+     * @param to The proposed next state.
+     * @param actorIdx Index of the actor who signed this transition.
+     */
+    function validTransition(
+        Channel.Params calldata params,
+        Channel.State calldata from,
+        Channel.State calldata to,
+        uint256 actorIdx)
+    external pure
+    {
+        require(from.appData.length == 0, "appData not needed in PaymentApp");
+        require(to.appData.length == 0, "appData not needed in PaymentApp");
+        // SubAllocs currently not implemented
+        require(from.outcome.locked.length == 0, "SubAlloc currently not implemented");
+        require(to.outcome.locked.length == 0, "SubAlloc currently not implemented");
+
+        for (uint256 i = 0; i < from.outcome.balances.length; i++) {
+            for (uint256 k = 0; k < from.outcome.balances[i].length; k++) {
+                // only sender can send money
+                if(k == actorIdx)
+                    require(to.outcome.balances[i][k] <= from.outcome.balances[i][k], "only sender can send money");
+                else
+                    require(to.outcome.balances[i][k] >= from.outcome.balances[i][k], "only non-sender can receive money");
+            }
+        }
+    }
 }
